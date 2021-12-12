f1df506e47681fcec1b3e0fc8abf7dec1307a952
hakan-erdogan-1 <hakanerdogan5138@hotmail.com>
2021-12-12 23:45:18 +0300

    Add project files.

---
+++ Calisma/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calisma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun1.Adi = "Karpuz";
+            urun1.Fiyati = 70;
+            urun1.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------");
+            }
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        //metot tanımlama
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi : " + urun.Adi );
+        }
+    }
+}
